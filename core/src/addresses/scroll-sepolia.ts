79edf73d57e3c97da9f32a9f3a746e21aee25754
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-02-20 20:26:42 +0100

    mod: added test net

--- core/src/addresses/scroll-sepolia.ts
+++ core/src/addresses/scroll-sepolia.ts
@@ -3,56 +3,56 @@ export const SCROLL_SEOLIA_SCROLL_ROLLUP = "0x2D567EcE699Eabe5afCd141eDB7A4f2D0D
 
 
 // ETH and ERC20 Bridge
-export const L1GatewayRouter = "0x13FBE0D0e5552b8c9c4AE9e2435F38f37355998a";
-export const L2GatewayRouter = "0x9aD3c5617eCAa556d6E166787A97081907171230";
+export const L1GatewayRouter_testnet = "0x13FBE0D0e5552b8c9c4AE9e2435F38f37355998a";
+export const L2GatewayRouter_testnet = "0x9aD3c5617eCAa556d6E166787A97081907171230";
 
 
 // Advanced Bridge Contracts
 
 
 // Scroll Messenger
-export const L1ScrollMessenger = "0x50c7d3e7f7c656493D1D76aaa1a836CedfCBB16A";
-export const L2ScrollMessenger = "0xBa50f5340FB9F3Bd074bD638c9BE13eCB36E603d";
+export const L1ScrollMessenge_testnet = "0x50c7d3e7f7c656493D1D76aaa1a836CedfCBB16A";
+export const L2ScrollMessenge_testnet = "0xBa50f5340FB9F3Bd074bD638c9BE13eCB36E603d";
 
 
 // ETH Bridge
-export const L1ETHGateway = "0x8A54A2347Da2562917304141ab67324615e9866d";
-export const L2ETHGateway = "0x91e8ADDFe1358aCa5314c644312d38237fC1101C";
-export const L1WETHGateway = "0x3dA0BF44814cfC678376b3311838272158211695";
-export const L2WETHGateway = "0x481B20A927206aF7A754dB8b904B052e2781ea27";
+export const L1ETHGateway_testnet = "0x8A54A2347Da2562917304141ab67324615e9866d";
+export const L2ETHGateway_testnet = "0x91e8ADDFe1358aCa5314c644312d38237fC1101C";
+export const L1WETHGateway_testnet = "0x3dA0BF44814cfC678376b3311838272158211695";
+export const L2WETHGateway_testnet = "0x481B20A927206aF7A754dB8b904B052e2781ea27";
 
 
 // ERC20 Bridge
-export const L1StandardERC20Gateway = "0x65D123d6389b900d954677c26327bfc1C3e88A13";
-export const L2StandardERC20Gateway = "0xaDcA915971A336EA2f5b567e662F5bd74AEf9582";
-export const L1CustomERC20Gateway = "0x31C994F2017E71b82fd4D8118F140c81215bbb37";
-export const L2CustomERC20Gateway = "0x058dec71E53079F9ED053F3a0bBca877F6f3eAcf";
+export const L1StandardERC20Gateway_testnet = "0x65D123d6389b900d954677c26327bfc1C3e88A13";
+export const L2StandardERC20Gateway_testnet = "0xaDcA915971A336EA2f5b567e662F5bd74AEf9582";
+export const L1CustomERC20Gateway_testnet = "0x31C994F2017E71b82fd4D8118F140c81215bbb37";
+export const L2CustomERC20Gateway_testnet = "0x058dec71E53079F9ED053F3a0bBca877F6f3eAcf";
 
 
 // ERC721 Bridge
-export const L1ERC721Gateway = "0xEF27A5E63aa3f1B8312f744b9b4DcEB910Ba77AC";
-export const L2ERC721Gateway = "0x179B9415194B67DC3c0b8760E075cD4415785c97";
+export const L1ERC721Gateway_testnet = "0xEF27A5E63aa3f1B8312f744b9b4DcEB910Ba77AC";
+export const L2ERC721Gateway_testnet = "0x179B9415194B67DC3c0b8760E075cD4415785c97";
 
 
 
 
 // ERC1155 Bridge
-export const L1ERC1155Gateway = "0xa5Df8530766A85936EE3E139dECE3bF081c83146";
-export const L2ERC1155Gateway = "0xe17C9b9C66FAF07753cdB04316D09f52144612A5";
+export const L1ERC1155Gateway_testnet = "0xa5Df8530766A85936EE3E139dECE3bF081c83146";
+export const L2ERC1155Gateway_testnet = "0xe17C9b9C66FAF07753cdB04316D09f52144612A5";
 
 
 
 
 // Gas Oracle (deployed on Sepolia)
-export const L1MessageQueueWithGasPriceOracle = "0xF0B2293F5D834eAe920c6974D50957A1732de763";
-export const L2GasPriceOracle = "0x247969F4fad93a33d4826046bc3eAE0D36BdE548";
+export const L1MessageQueueWithGasPriceOracle_testnet = "0xF0B2293F5D834eAe920c6974D50957A1732de763";
+export const L2GasPriceOracle_testnet = "0x247969F4fad93a33d4826046bc3eAE0D36BdE548";
 
 
 
 
 // L2 Predeploys
-export const L1MessageQueue = "0x5300000000000000000000000000000000000000";
-export const L1GasPriceOracle = "0x5300000000000000000000000000000000000002";
-export const Whitelist = "0x5300000000000000000000000000000000000003";
-export const WrappedEther = "0x5300000000000000000000000000000000000004";
-export const L2TxFeeVault = "0x5300000000000000000000000000000000000005";
+export const L1MessageQueue_testnet = "0x5300000000000000000000000000000000000000";
+export const L1GasPriceOracle_testnet = "0x5300000000000000000000000000000000000002";
+export const Whitelist_testnet = "0x5300000000000000000000000000000000000003";
+export const WrappedEther_testnet = "0x5300000000000000000000000000000000000004";
+export const L2TxFeeVault_testnet = "0x5300000000000000000000000000000000000005";

5c15e0fce61a97720aed8155af449a3b1a4b3dca
Lamsy <olamide.adetula.1744@gmail.com>
2024-02-20 20:21:12 +0100

    abi deleted

---
+++ core/src/cross-chain-interaction.ts
@@ -0,0 +1,2 @@
+import { serialize, UnsignedTransaction } from '@ethersproject/transactions'
+import { BigNumberish, BytesLike, ContractTransaction, ethers, Provider, Signer, Contract } from 'ethers'

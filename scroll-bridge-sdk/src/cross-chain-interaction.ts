832f3a229480aa306b03dc66cdf39eff2b5b0468
Nwele Uchenna (developeruche) <nweleuchenna2004@gmail.com>
2024-02-24 00:10:51 +0100

    mod: done with the interface creation for cross chain messaging

--- scroll-bridge-sdk/src/cross-chain-interaction.ts
+++ scroll-bridge-sdk/src/cross-chain-interaction.ts
@@ -1,5 +1,5 @@
 import { CHAIN, MESSAGE_STATUS, relayMessageParams } from "./types";
-
+import { ethers, Provider, Signer } from 'ethers';
 
 
 
@@ -14,7 +14,7 @@ import { CHAIN, MESSAGE_STATUS, relayMessageParams } from "./types";
  * @param {number} gasLimit - this is the gas limit for the transaction (in wei) (when excecuting the message in destination chain) (this is important when messeage route is from L1 to L2)
  * @param {string} refundAddress - this is the address the remaining gas for excecution would be sent to
  * @param {number} fee - this is the fee for sending the message
- * @param {}
+ * @param {Signer} signer - this is the signer for the transaction
  * @returns {Promise<string>} - this is the message hash
 */
 export async function sendMessage(
@@ -24,7 +24,8 @@ export async function sendMessage(
     data: string, 
     gasLimit: number, 
     from: string, 
-    fee: number
+    fee: number,
+    signer: Signer
     ): Promise<string>
 {
     return '';
@@ -35,10 +36,11 @@ export async function sendMessage(
  * 
  * @description this function would be used to estimate the fee for sending a message from 'Chain A' to 'Chain B'
  * @param {number} gasLimit - this is the gas limit for the transaction (in wei) (when excecuting the message in destination chain) (this is important when messeage route is from L1 to L2)
+ * @param {Provider} provier - this is the provider for the transaction
  * @returns {number} - this is the estimated fee for sending the message
  * @notice this function is only necessary when sending a message from L1 to L2
  */
-export async function estimateCrossDomainMessageFee(gasLimit: number): Promise<number> {
+export async function estimateCrossDomainMessageFee(gasLimit: number, provier: Provider): Promise<number> {
     return 1;
 }
 
@@ -48,9 +50,11 @@ export async function estimateCrossDomainMessageFee(gasLimit: number): Promise<n
  * @description this function is used to get the message status
  * @param {string} messageHash - this is the hash of the message
  * @param {CHAIN} chain - this is the chain the message was sent from
+ * @param {Provider} provider - this is the provider for the transaction
+ * 
  * @returns {Promise<MESSAGE_STATUS>} - this is the status of the message
  */
-export async function getMessageStatus(soure: CHAIN, messageHash: string): Promise<MESSAGE_STATUS> {
+export async function getMessageStatus(soure: CHAIN, messageHash: string, provider: Provider): Promise<MESSAGE_STATUS> {
     return MESSAGE_STATUS.PENDING;
 }
 
@@ -77,8 +81,9 @@ export async function getMessageProof(txHash: string): Promise<any> {
 /**
  * This function is used to call the relayMessage function, this last step in the L2 -> L1 route
  * @param {relayMessageParams} - this is the parameters for the relayMessage function
+ * @param {Signer} - this is the signer for the transaction
  */
 
-export async function relayMessage(params: relayMessageParams): Promise<any> {
+export async function relayMessage(params: relayMessageParams, signer: Signer): Promise<any> {
 
 }

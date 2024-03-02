ea5246b99813d169e62d177039741721e74e1b65
casweeney <casweeno2000@gmail.com>
2024-03-02 14:46:03 +0100

    FIXES: abstracted urls to env file and added .env to gitignore

--- scroll-bridge-sdk/test/erc20.spec.ts
+++ scroll-bridge-sdk/test/erc20.spec.ts
@@ -2,10 +2,14 @@ import { depositERC20, withdrawERC20 } from '@src/assets-bridge/erc20'
 import { ethers } from 'ethers'
 import { config } from 'dotenv'
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+config()
 
-let providerL1 = new ethers.JsonRpcProvider('https://eth-sepolia.g.alchemy.com/v2/Ytq0aV34dWOA9X6gWhl_6trwmUTb58Ip')
-let providerL2 = new ethers.JsonRpcProvider('https://sepolia-rpc.scroll.io')
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const SCROLL_RPC_URL = process.env.SCROLL_RPC_URL
+
+let providerL1 = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
+let providerL2 = new ethers.JsonRpcProvider(SCROLL_RPC_URL)
 
 let walletL1 = new ethers.Wallet(PRIVATE_KEY!, providerL1)
 let walletL2 = new ethers.Wallet(PRIVATE_KEY!, providerL2)

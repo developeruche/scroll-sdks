17dfff6f78c5d98a40e5f59e0475116ca2ad4fcf
Lamsy <olamide.adetula.1744@gmail.com>
2024-03-05 09:56:43 +0100

    L2 -L1 message testing

--- scroll-bridge-sdk/src/assets-bridge/erc721.ts
+++ scroll-bridge-sdk/src/assets-bridge/erc721.ts
@@ -1,19 +1,9 @@
-import { BigNumberish, Signer } from "ethers";
-import { SendMessageResponse } from "../types";
-import { genL1ERC721Gateway, genL2ERC721Gateway } from "@scroll-tech/core";
-
-
-
-
-
-
-
-
-
-
+import { BigNumberish, Provider, Signer } from 'ethers'
+import { SendMessageResponse } from '../types'
+import { genL1ERC721Gateway, genL2ERC721Gateway } from '@scroll-tech/core'
 
 /**
- * 
+ *
  * @param target this is the address of the NFT token on L2
  * @param recipent this is the address that would be recieving the NFT on L2
  * @param tokenId this is the token ID of the NFT to be deposited
@@ -21,89 +11,77 @@ import { genL1ERC721Gateway, genL2ERC721Gateway } from "@scroll-tech/core";
  * @param vaule this is the vaule this Tx would go along with (amount + fee) [use the core moudle to estimate the fee]
  * @param signer this the signer of the transaction
  * @param {boolean} isTestnet this is a flag to indicate if the network is a testnet or not
- * @returns {SendMessageResponse} {txHash: string, messageHash: string} 
+ * @returns {SendMessageResponse} {txHash: string, messageHash: string}
  */
-export async function depositERC721( 
-    target: string, 
-    recipent: string, 
-    tokenId: BigNumberish, 
-    gasLimit: number, 
-    value: BigNumberish, 
-    signer: Signer, 
-    isTestnet: boolean 
+export async function depositERC721(
+  target: string,
+  recipent: string,
+  tokenId: BigNumberish,
+  gasLimit: number,
+  value: BigNumberish,
+  signer: Signer,
+  isTestnet: boolean
 ): Promise<SendMessageResponse> {
+  let l1ERC721gateway = genL1ERC721Gateway(signer, isTestnet)
 
+  let depositERC721 = await l1ERC721gateway['depositERC721(address,address,uint256,uint256)'](
+    target,
+    recipent,
+    tokenId,
+    gasLimit, // gas limit
+    {
+      value,
+    }
+  )
 
-    let l1ERC721gateway = genL1ERC721Gateway(signer, isTestnet);
+  await depositERC721.wait()
 
+  const messageResponse: SendMessageResponse = {
+    txHash: depositERC721.hash,
+    messageHash: '',
+  }
 
-    let depositERC721 = await l1ERC721gateway["depositERC721(address,address,uint256,uint256)"](
-        target,
-        recipent,
-        tokenId,
-        gasLimit, // gas limit
-        {
-            value,
-        }
-    )
-
-    await  depositERC721.wait()
-
-
-    const messageResponse: SendMessageResponse = {
-        txHash: depositERC721.hash,
-        messageHash: ""
-    };
-
-    return messageResponse;
+  return messageResponse
 }
 
-
-
 /**
- * 
- * @param target this is the address of the NFT token on L2 
- * @param recipent this is the address that would be recieving the NFT on L1 
- * @param tokenId this is the token ID of the NFT to be withdrawn 
- * @param gasLimit this is the gas limit for the transaction 
- * @param value the vaule this Tx would go along with (amount + fee) [in this case fee can be 0, use the core moudle to estimate the fee] 
+ *
+ * @param target this is the address of the NFT token on L2
+ * @param recipent this is the address that would be recieving the NFT on L1
+ * @param tokenId this is the token ID of the NFT to be withdrawn
+ * @param gasLimit this is the gas limit for the transaction
+ * @param value the vaule this Tx would go along with (amount + fee) [in this case fee can be 0, use the core moudle to estimate the fee]
  * @param signer this the signer of the transaction
- * @param {boolean} isTestnet this is a flag to indicate if the network is a testnet or not 
- * @returns {SendMessageResponse} {txHash: string, messageHash: string}   
- */  
+ * @param {boolean} isTestnet this is a flag to indicate if the network is a testnet or not
+ * @returns {SendMessageResponse} {txHash: string, messageHash: string}
+ */
 export async function withdrawERC721(
-    target: string, 
-    recipent: string, 
-    tokenId: BigNumberish,   
-    gasLimit: number,   
-    value: BigNumberish,   
-    signer: Signer,   
-    isTestnet: boolean   
+  target: string,
+  recipent: string,
+  tokenId: BigNumberish,
+  gasLimit: number,
+  value: BigNumberish,
+  signer: Provider,
+  isTestnet: boolean
 ): Promise<SendMessageResponse> {
+  let l2ERC721gateway = genL2ERC721Gateway(signer, isTestnet)
 
-    
-    let l2ERC721gateway = genL2ERC721Gateway(signer, isTestnet);
+  let withdrawERC721 = await l2ERC721gateway['withdrawERC721(address,address,uint256,uint256)'](
+    target,
+    recipent,
+    tokenId,
+    gasLimit, // gas limit
+    {
+      value,
+    }
+  )
 
+  await withdrawERC721.wait()
 
-    let withdrawERC721 = await l2ERC721gateway["withdrawERC721(address,address,uint256,uint256)"](
-        target,
-        recipent,
-        tokenId,
-        gasLimit, // gas limit
-        {
-            value,
-        }
-    )
+  const messageResponse: SendMessageResponse = {
+    txHash: withdrawERC721.hash,
+    messageHash: '',
+  }
 
-    await  withdrawERC721.wait()
-
-
-
-
-    const messageResponse: SendMessageResponse = {
-        txHash: withdrawERC721.hash,
-        messageHash: ""
-    };
-
-    return messageResponse;
+  return messageResponse
 }
--- scroll-bridge-sdk/test/messaging.spec.ts
+++ scroll-bridge-sdk/test/messaging.spec.ts
@@ -1,14 +1,37 @@
 import { ethers } from 'ethers'
 import { config } from 'dotenv'
+import { sendMessage, replayMessage, relayMessageWithProof, dropMessage } from '@src/crossChainMessaging'
+import { CHAIN } from '@src/types'
 
 config()
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const SCROLL_RPC_URL = process.env.SCROLL_RPC_URL
+let targetContract = '0x0E8ceC32Cbaf8a16Dbc1824870C404d22746f990'
 
 const providerL1 = new ethers.JsonRpcProvider(SEPOLIA_RPC_URL)
 const providerL2 = new ethers.JsonRpcProvider(SCROLL_RPC_URL)
 
 const walletL1 = new ethers.Wallet(PRIVATE_KEY!, providerL1)
 const walletL2 = new ethers.Wallet(PRIVATE_KEY!, providerL2)
+const isTestnet = true
+
+describe('L2 to L1 Message ', async () => {
+  describe('sendMessage', async () => {
+    it('should send message', async () => {
+      let l1Messenger = await sendMessage(
+        CHAIN.L2,
+        targetContract,
+        0,
+        '0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001754657374696e6720666f722073656e644d657373616765000000000000000000',
+        10000000,
+        walletL2.address,
+        0,
+        walletL2,
+        isTestnet
+      )
+      console.log('L1 to L2 Message', l1Messenger.txHash)
+    })
+  })
+})
